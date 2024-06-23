e49169d0981e5b6c1bd80ff9911c1dee4a974f76
Arcade Kakpo <arcadej@hotmail.fr>
2024-06-23 00:30:29 +0000

    Add javascript code to automate contact

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const websites = [
         { name: "Love Maths", url: "https://arcadejk.github.io/love-math/", image: "assets/images/website1.png" },
-        { name: "Website 2", url: "https://arcadejk.github.io/movies-reacts-playlist/", image: "assets/images/website2.png" },
-        { name: "Website 3", url: "https://loves-sandwiches-arcade-5cd3a5ebfca2.herokuapp.com/", image: "assets/images/website3.png" },
-        { name: "Website 4", url: "https://arcadejk.github.io/Love-Running-Project/", image: "assets/images/website4.png" },
+        { name: "Korean Movie Reacts", url: "https://arcadejk.github.io/movies-reacts-playlist/", image: "assets/images/website2.png" },
+        { name: "Love Sandwiches", url: "https://loves-sandwiches-arcade-5cd3a5ebfca2.herokuapp.com/", image: "assets/images/website3.png" },
+        { name: "Love Running", url: "https://arcadejk.github.io/Love-Running-Project/", image: "assets/images/website4.png" },
         { name: "K-Drama Fandom", url: "https://arcadejk.github.io/kdrama-fandom/", image: "assets/images/website5.png" },
-        { name: "Website 6", url: "https://arcadejk.github.io/bootstrap4/", image: "assets/images/website6.png" },
-        { name: "Website 7", url: "https://love-letters-and-grammar-5f96e9223b1d.herokuapp.com/", image: "assets/images/website7.png" },
+        { name: "Whiskey Drop website", url: "https://arcadejk.github.io/bootstrap4/", image: "assets/images/website6.png" },
+        { name: "Love Letters and Grammar", url: "https://love-letters-and-grammar-5f96e9223b1d.herokuapp.com/", image: "assets/images/website7.png" },
     ];
 
     const websitesList = document.getElementById('websites-list');
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         text.textContent = site.name;
 
         listItem.appendChild(link);
-        listItem.appendChild(text);
+    
         websitesList.appendChild(listItem);
     });
 });
